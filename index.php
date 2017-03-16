486f2ea9b7cf20159fee97e32420855801c2ddf7
muchilafred <f.m.muchila@rgu.ac.uk>
2017-03-16 11:01:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,18 @@
 </head>
 <body>
 <header>
-    <h1>Marvel Movies</h1>
+    <h1>Marvel Movies Lab activity</h1>
 </header>
 <main>
         <?
         include 'dbconnect.php';
         ?>
 
-        <p><a href="movie list.php">All Marvel Movies</a></p>
-        <p><a href="xmen.php">All X-MEN Movies</a></p>
+        <p><a href="movie list.php">List all Marvel Movies</a></p>
+        <p><a href="marvel-studios.php">Movies by Marvel Studios</a></p>
         <p><a href="movies2010.php">Movies After 2010</a></p>
+        <p><a href="xmen.php">List X-MEN Movies</a></p>
+        
         
 
 
